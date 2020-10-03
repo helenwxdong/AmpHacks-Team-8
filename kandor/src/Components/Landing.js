9d2f098b7d25501c701e8836092093821b56fbe4
helenwxdong <helenwxdong@gmail.com>
2020-10-03 17:01:19 -0400

    Installed bootstrap

--- kandor/src/Components/Landing.js
+++ kandor/src/Components/Landing.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 export default class Landing extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isHealth : false,
+            isPolitics : false,
+            isArt : false,
+            isBooks : false,
+            isFitness : false,
+            isCareer : false,
+            isChildren : false,
+        }
+    }
     render() {
       return (
         <div>
-            <h1>What would you like to chat about? :)))</h1>
+            <h1>Kandor. Fostering a community for you.</h1>
         </div>
       );
     }
