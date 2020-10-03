b941a9bf2b07f827757e085c366185f2550eddd1
helenwxdong <helenwxdong@gmail.com>
2020-10-03 15:36:20 -0400

    Created basic components

--- kandor/src/App.js
+++ kandor/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from './Components/Landing.js';
 
 function App() {
-  return <div className='App'>Kandor</div>;
+  return <div className='App'>Kandor
+  <Landing/>
+  </div>;
 }
 
 export default App;
---
+++ kandor/src/Components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Landing extends React.Component {
+    render() {
+      return (
+        <div>
+            <h1>What would you like to chat about? :)))</h1>
+        </div>
+      );
+    }
+  }
