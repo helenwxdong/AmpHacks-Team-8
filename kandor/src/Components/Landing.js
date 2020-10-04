d9169c1029b1f1c40bf31432bd6b0804e2b6c2d4
Randy Shao <shao.randy@gmail.com>
2020-10-03 23:33:49 -0400

    Change

--- kandor/src/Components/Landing.js
+++ kandor/src/Components/Landing.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Logo from '../img/kandor.png';
 import Topics from './Topics.js';
 
@@ -21,32 +20,3 @@ const Landing = () => {
 };
 
 export default Landing;
-=======
-import Button from 'react-bootstrap/Button';
-
-export default class Landing extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            isHealth : false,
-            isPolitics : false,
-            isArt : false,
-            isBooks : false,
-            isFitness : false,
-            isCareer : false,
-            isChildren : false,
-        }
-    }
-    render() {
-      return (
-        <div>
-            <h1>Kandor. Fostering a community for you.</h1>
-            <h1>What would you like to chat about today?</h1>
-            <Button variant="primary" block>
-                Block level button
-            </Button>
-        </div>
-      );
-    }
-  }
->>>>>>> 1998e77a7c721b86bed40e4f303c15217aa606e7
